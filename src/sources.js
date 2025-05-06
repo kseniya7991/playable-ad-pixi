3f527fb15032880eefe31e6a648830863b237076
kseniya-st <kseniya.stoichykava@alreadymedia.com>
2025-05-06 22:31:57 +0300

    add resources to load

--- src/sources.js
+++ src/sources.js
@@ -23,4 +23,28 @@ export default [
         alias: "spineAtlas",
         src: "https://raw.githubusercontent.com/pixijs/spine-v8/main/examples/assets/spineboy-pma.atlas",
     },
+    {
+        alias: "fireworksSprite",
+        src: "/assets/fireworks.json"
+    },
+    {
+        alias:"chipBlue",
+        src:"/assets/ch_blue.png"
+    },
+    {
+        alias:"chipGreen",
+        src:"/assets/ch_green.png"
+    },
+    {
+        alias:"chipRed",
+        src:"/assets/ch_red.png"
+    },
+    {
+        alias:"chipYellow",
+        src:"/assets/ch_yellow.png"
+    },
+    {
+        alias:"chipPink",
+        src:"/assets/ch_pink.png"
+    }
 ];
